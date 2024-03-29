f16c43ecdcfbc481602a48f91a64783b421919e9
KimJS0328 <wjddkel328@gmail.com>
2024-03-29 15:55:56 +0900

    style: indentation space 통일

--- src/test/java/racing/domain/CarFactoryTest.java
+++ src/test/java/racing/domain/CarFactoryTest.java
@@ -10,16 +10,16 @@ import org.junit.jupiter.api.Test;
 import racing.infra.RandomCarEngine;
 
 public class CarFactoryTest {
-	@Test
-	void 유효한_이름들() {
+    @Test
+    void 유효한_이름들() {
 
-		CarFactory factory = new CarFactory(new RandomCarEngine());
-		List<Car> cars = factory.fromCsvNames("vec,amber,sage");
+        CarFactory factory = new CarFactory(new RandomCarEngine());
+        List<Car> cars = factory.fromCsvNames("vec,amber,sage");
 
-		List<String> names = cars.stream()
-			.map(Car::getName)
-			.collect(Collectors.toList());
+        List<String> names = cars.stream()
+            .map(Car::getName)
+            .collect(Collectors.toList());
 
-		assertThat(names).containsExactlyInAnyOrder("vec", "amber", "sage");
-	}
+        assertThat(names).containsExactlyInAnyOrder("vec", "amber", "sage");
+    }
 }
